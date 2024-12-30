2250b10b0adb2e27b556b15e617fc952f92cb106
Steve Auxter <116984055+SteveAuxter@users.noreply.github.com>
2024-12-30 10:53:04 -0500

    Initial upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <title></title>
+</head>
+<body>
+    <center>
+        <hr>
+        <h3>Thank you for using Chromebooks On Demand (CBOD) v2</h3>
+        <a href="https://github.com/SteveAuxter/CBODv2">CBODv2 on Github</a> | &copy Steve Auxter 2025
+    </center>
+</body>
+</html>
