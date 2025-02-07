7689833eb5d48238cd49dc3744248e56ff04f334
Steve Auxter <116984055+SteveAuxter@users.noreply.github.com>
2025-02-07 13:48:24 -0500

    Update index.php
    Removed unnecessary lines

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="mystyle.css">
-    <link rel="icon" type="image/x-icon" href="favicon/favicon.ico">
-    <title>REDIRECTING...</title>
-</head>
-<body>
-    <?php header("Location: search_main.php"); exit(); ?>
-</body>
-</html>
+<?php header("Location: search_main.php"); exit(); ?>
